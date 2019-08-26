3d4768f182c50a77dc6f15684f82beb59b8a4802
Klaus Landsdorf <klaus@bianco-royal.de>
2019-08-26 14:49:12 +0200

    chore: add linter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'semistandard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
